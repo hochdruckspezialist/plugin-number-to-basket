d80a0cf1741f066c60b8d0b4705744b02413fd40
Dominik Meyer <dominik.meyer@plentymarkets.com>
2017-03-15 12:14:53 +0100

    UPDATE

--- src/Providers/NumberToBasketServiceProvider.php
+++ src/Providers/NumberToBasketServiceProvider.php
@@ -3,11 +3,12 @@
 namespace NumberToBasket\Providers;
 
 use Plenty\Plugin\ServiceProvider;
+use NumberToBasket\Providers\NumberToBasketRouteServiceProvider;
 
 class NumberToBasketServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        
+        $this->getApplication()->register(NumberToBasketRouteServiceProvider::class);
     }
 }
